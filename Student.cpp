61263ced84645e42ed61ee795169382f6c5342a6
411748 <411748@bsd48.org>
2024-01-05 12:54:08 -0800

    Added code for Student

--- Student.cpp
+++ Student.cpp
@@ -1,7 +1,36 @@
-//Student.cpp
-
-
 #include "Student.h"
+
+student::student(const char* firstName, const char* lastName, int studentID, double GPA) {
+    this->firstName = new char[strlen(firstName) + 1];
+    strcpy(this->firstName, firstName);
+
+    this->lastName = new char[strlen(lastName) + 1];
+    strcpy(this->lastName, lastName);
+
+    this->studentID = studentID;
+    this->GPA = GPA;
+}
+
+student::~student() {
+    delete[] firstName;
+    delete[] lastName;
+}
+
+const char* student::getFirstName() {
+    return firstName;
+}
+
+const char* student::getLastName() {
+    return lastName;
+}
+
+int student::getStudentID() {
+    return studentID;
+}
+
+double student::getGPA() {
+    return GPA;
+}
 
 
 
--- Student.h
+++ Student.h
@@ -1,3 +1,27 @@
-//Student.h
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <cstring>
+
+class student {
+public:
+    student(const char* firstName, const char* lastName, int studentID, double GPA);
+    ~student();
+    
+    const char* getFirstName();
+    const char* getLastName();
+    int getStudentID();
+    double getGPA();
+
+private:
+    char* firstName;
+    char* lastName;
+    int studentID;
+    double GPA;
+};
+
+#endif
+
 
 
